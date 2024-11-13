36f364ae95e7eca2789a5c12b8c0e81d63c24f16
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-11-13 09:24:23 +0100

    qdfghjk,.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={count ==0 ? 'bia' : 'cza'}>
+
+      <button onClick={() => setCount(count==0? count + 1: count - 1)}>zmień stan</button>
+
     </div>
   );
 }

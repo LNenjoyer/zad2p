e595aafa79ca7afde7b86e387180de27ac6e2ddc
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-11-13 10:03:42 +0100

    dokumentacjaAAAAAAAAAaaaaaaaaaaaaAAAAAAA

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
+  // zmienna count, nie rozumiem po co utrudniać życie; dziwne to
   return (
     <div className={count ===0 ? 'bia' : 'cza'}>
-
+      {/*dla wartości count 0 przyjmuje klase bia, a dla wartosci 1 klase cza*/}
       <button onClick={() => setCount(count===0? count + 1: count - 1)}>zmień stan</button>
-
+      {/*to if który zmienia stan na 1 i 0*/}
+        <h1>asfdgddgdggd</h1>
+      {/*random text który pokazuje że kolor tekstu też sie zmienia*/}
     </div>
   );
 }
